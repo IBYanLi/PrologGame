6850aeb517c93bc1105b865a93593c6d6b4f2e3a
ddtam <derek.tam.94@gmail.com>
2017-10-23 18:08:39 -0700

    implemented change_your_mind
    change_your_mind can be used after game-over messages to make a different decision instead of restarting from the beginning.
    
    In order for this to work, all future scenes must be logged into the sceneDirectory file.

---
+++ sceneDirectory.pl
@@ -0,0 +1,24 @@
+% directory for matching scene IDs to their function call
+prop(s001, scene, game_start).
+prop(s002, scene, leave).
+prop(s003, scene, stay).
+prop(s004, scene, fall_asleep).
+prop(s005, scene, leave).
+prop(s006, scene, run_away).
+prop(s007a, scene, investigate).
+prop(c007, scene, encounter).
+prop(s007b, scene, end_investigate).
+prop(s008, scene, proceed).
+prop(s009, scene, call).
+
+% directory for getting from one scene to another
+prop(s001, goto, s002).
+prop(s001, goto, s003).
+prop(s002, goto, s006).
+prop(s002, goto, s007a).
+prop(s003, goto, s004).
+prop(s003, goto, s005).
+prop(s006, goto, s009).
+prop(s007a, goto, c007).
+prop(c007, goto, s007b).
+prop(s007b, goto, s009).
